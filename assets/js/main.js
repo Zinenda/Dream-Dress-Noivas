e9729528e4282e44253b9bbf2f73af0908beec33
Ivo Zinenda <izinenda@gmail.com>
2018-01-18 16:52:45 +0200

    first look

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $(".dropdown-trigger").dropdown({
+        constrainWidth: false,
+    });
+    $('.slider').slider();
+    // Pause slider
+    $('.slider').slider('pause');
+    // Start slider
+    $('.slider').slider('start');
+    // Next slide
+    $('.slider').slider('next');
+    // Previous slide
+    $('.slider').slider('prev');
+    $('.tooltipped').tooltip();
+});
